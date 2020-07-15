455b390c47a150915e014ecacf3aa53d7083d306
Roky Das <droky136@gmail.com>
2020-07-15 15:26:45 +0600

    swapping updated with destructing

--- swapping.js
+++ swapping.js
@@ -21,3 +21,11 @@ num2 = num1 - num2;
 num1 = num1 - num2;
 
 console.log("After swapping Num1 is " + num1 + " and Num2 is " + num2);
+
+//Using destructing
+num1 = 25;
+num2 = 50;
+
+console.log("Before swapping Num1 is " + num1 + " and Num2 is " + num2);
+[num1, num2] = [num2, num1];
+console.log("After swapping Num1 is " + num1 + " and Num2 is " + num2);
